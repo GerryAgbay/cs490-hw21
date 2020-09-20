b48bea6e58eccf31925404d9d2111412ee57d936
EC2 Default User <ec2-user@ip-172-31-24-41.ec2.internal>
2020-09-20 19:47:07 +0000

    Adds .gitignore and updates server-side.py to hide twitter api keys and check if they work

--- server-side.py
+++ server-side.py
@@ -1,11 +1,27 @@
 import flask
 import os
+import tweepy
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Cursor
+import sys
+
+twitter_api_key = os.environ['TWITTER_API_KEY']
+twitter_api_key_secret = os.environ['TWITTER_API_KEY_SECRET']
+
+auth = OAuthHandler(twitter_api_key, twitter_api_key_secret)
+auth_api = API(auth)
+
+user = sys.argv[1]
+item = auth_api.get_user(user)
+print("Name: " + item.name)
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello, World!"
+)
     
 app.run(
     port=int(os.getenv('PORT', 8080)),
