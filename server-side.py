785e70eea163b8fcc304d867888a25be9d14a13f
EC2 Default User <ec2-user@ip-172-31-24-41.ec2.internal>
2020-09-21 02:37:19 +0000

    Modify server-side.py, which can now store ten tweets related to a hardcoded keyword (food) into a list and print a random one.

--- server-side.py
+++ server-side.py
@@ -5,6 +5,7 @@ from tweepy import OAuthHandler
 from tweepy import API
 from tweepy import Cursor
 import sys
+import random
 
 twitter_api_key = os.environ['TWITTER_API_KEY']
 twitter_api_key_secret = os.environ['TWITTER_API_KEY_SECRET']
@@ -12,16 +13,26 @@ twitter_api_key_secret = os.environ['TWITTER_API_KEY_SECRET']
 auth = OAuthHandler(twitter_api_key, twitter_api_key_secret)
 auth_api = API(auth)
 
-user = sys.argv[1]
-item = auth_api.get_user(user)
-print("Name: " + item.name)
+text_query = "food"
+tweet_list = []
+count = 10
+
+def get_tweets():
+    lang = "en"
+
+    for tweet in auth_api.search(text_query, lang, count):
+        tweet_list.append(f"{tweet.user.screen_name}  -----  {tweet.text}")
+        #print(tweet.user.name + " ----- " + tweet.text)
+
+get_tweets()
+rand_idx = random.randint(0, 9)
+print(tweet_list[rand_idx])
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello, World!"
-)
     
 app.run(
     port=int(os.getenv('PORT', 8080)),
