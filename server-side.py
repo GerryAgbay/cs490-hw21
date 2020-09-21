6cbde84382c9c675f73da545874af468537b8daf
EC2 Default User <ec2-user@ip-172-31-24-41.ec2.internal>
2020-09-21 05:40:21 +0000

    Add templates/food_tweets.html and update server-side.py to display the tweet on the terminal and on a separate page

--- server-side.py
+++ server-side.py
@@ -13,36 +13,46 @@ twitter_api_key_secret = os.environ['TWITTER_API_KEY_SECRET']
 auth = OAuthHandler(twitter_api_key, twitter_api_key_secret)
 auth_api = API(auth)
 
-if (len(sys.argv) > 1):
+
+if (len(sys.argv) > 1):\
     search_list = sys.argv[1:]
 else:
     print("Please provide a list of foods at the command line.")
     sys.exit(0)
+    
 
 def get_item():
-    index = random.randint(0, len(search_list))
+    index = random.randint(0, len(search_list)-1)
     search_item = search_list[index]
     return search_item
     
+
 tweet_list = []
 count = 10
+lang = "en"
+search = get_item()
 
-def get_tweets():
-    lang = "en"
+for tweet in Cursor(auth_api.search, search, lang, tweet_mode='extended').items(count):
+    screen_name = tweet.user.screen_name
+    contents = tweet.full_text
+    date = tweet.created_at
+    tweet_list.append(f"{screen_name}   -----   {date}   -----   {contents}")
 
-    for tweet in auth_api.search(get_item(), lang, count):
-        tweet_list.append(f"{tweet.user.screen_name}  -----  {tweet.text}")
-        #print(tweet.user.name + " ----- " + tweet.text)
 
-get_tweets()
 rand_idx = random.randint(0, 9)
 print(tweet_list[rand_idx])
+
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    info = tweet_list[rand_idx]
+    return flask.render_template(
+        "food_tweets.html",
+        information = info,
+        keyword = search
+    )
     
 app.run(
     port=int(os.getenv('PORT', 8080)),
