f23d48e0625dbb4f4f30042c1055343e96061030
GerryAgbay <gerryagbayjr@gmail.com>
2020-09-28 15:57:05 +0000

    Retrieves a random recipe given hardcoded food items and prints id, name, original source, ready in minutes, and image url of the recipe in terminal

--- server-side.py
+++ server-side.py
@@ -6,6 +6,7 @@ import random
 import requests
 from os.path import join, dirname
 from dotenv import load_dotenv
+import json
 
 dotenv_path = join(dirname(__file__), 'twitter.env')
 load_dotenv(dotenv_path)
@@ -22,25 +23,47 @@ auth_api = tweepy.API(auth)
 dotenv_path = join(dirname(__file__), 'spoonacular.env')
 load_dotenv(dotenv_path)
 
-spoonacular_key = os.getenv('SPOONACULAR_KEY')
-url = 'https://api.spoonacular.com/recipes/search?apiKey={}'.format(spoonacular_key)
-
-response = requests.get(url)
-json_body = response.json()
-print(json_body)
-
 #if (len(sys.argv) > 1):\
     #search_list = sys.argv[1:]
 #else:
     #print("Please provide a list of foods at the command line.")
     #sys.exit(0)
     
-search_list = ["sushi", "onigiri", "pho", "udon", "kimchi", "tonkatsu", "bulgogi", 
-               "wonton", "yakitori", "mochi", "jiaozi", "sashimi", "ramen", "yakiniku", "wagashi"]
+search_list = ["hamburger", "spaghetti", "potatoes", "bread"]
+    
+#search_list = ["sushi", "onigiri", "pho", "udon", "kimchi", "tonkatsu", "bulgogi", 
+               #"wonton", "yakitori", "mochi", "jiaozi", "sashimi", "ramen", "yakiniku", "wagashi"]
 
 def get_food():
     search_item = random.choice(search_list)
     return search_item
+    
+food_name = get_food()
+    
+spoonacular_key = os.getenv('SPOONACULAR_KEY')
+id_url = "https://api.spoonacular.com/recipes/complexSearch?query=" + food_name + "&number=10&apiKey=" + spoonacular_key
+
+id_response = requests.request("GET", id_url)
+id_json_body = id_response.json()
+#print(id_json_body)
+id_dictionary = id_json_body["results"][random.randint(0,9)]
+#print(id_dictionary)
+recipe_id = id_dictionary["id"]
+
+recipe_url =  "https://api.spoonacular.com/recipes/" + str(recipe_id) + "/information?includeNutrition=false&apiKey=" + spoonacular_key
+recipe_response = requests.request("GET", recipe_url)
+recipe_json_body = recipe_response.json()
+
+recipe_name = recipe_json_body["title"]
+recipe_link = recipe_json_body["sourceUrl"]
+recipe_time = recipe_json_body["readyInMinutes"]
+recipe_image = recipe_json_body["image"]
+
+print("ID: " + str(recipe_id))
+print("Name: " + recipe_name)
+print("Original Source: " + recipe_link)
+print("Ready in (Minutes): " + str(recipe_time))
+print("Image URL: " + recipe_image)
 
 def get_tweets():
     tweets_list = []
